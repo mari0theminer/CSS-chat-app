303055f569eac410b2bd00b689890ef5539cf9bf
Mari0theminer <dev@unir.wtf>
2020-12-23 12:31:37 +0100

    fixed redirect

--- app/src/Security/LoginFormAuthenticator.php
+++ app/src/Security/LoginFormAuthenticator.php
@@ -97,7 +97,7 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator implements P
         }
 
         // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        return $this->urlGenerator->generate('home');
+        return new RedirectResponse($this->urlGenerator->generate('home'));
     }
 
     protected function getLoginUrl()
