51720ce0fabfdc362c5993b867d0b281ba4b8d24
Mari0 <mari0@mari0.dev>
2021-01-14 15:21:28 +0100

    fixed register

--- app/deploy.sh
+++ app/deploy.sh
@@ -1,5 +1,5 @@
-cp /opt/web/css-save/.env /opt/web/CSS-chat-app/app/
-cp /opt/web/css-save/docker-compose.yml /opt/web/CSS-chat-app/
+cp -rf /opt/web/css-save/.env /opt/web/CSS-chat-app/app/
+cp  -rf /opt/web/css-save/docker-compose.yml /opt/web/CSS-chat-app/
 cd /opt/web/CSS-chat-app/
 docker-compose exec php-fpm php bin/console do:mi:mi -q
 docker-compose restart
